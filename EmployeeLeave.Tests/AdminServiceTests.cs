2f3def4473aad3148939eb032fb9ae8d47721fc8
Tejas Tyagi <tyagite@sheridancollege.ca>
2025-12-14 01:23:51 -0500

    Fixed errors

--- EmployeeLeave.Tests/AdminServiceTests.cs
+++ EmployeeLeave.Tests/AdminServiceTests.cs
@@ -1,10 +1,9 @@
 ﻿namespace EmployeeLeave.Tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+// public class UnitTest1
+// {
+//     // public void Test1()
+//     // {
 
-    }
-}
+//     // }
+// }
